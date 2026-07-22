76b354b72b7541115c8c5a79f14538370e65e1d4
Craig <1188869+cscd98@users.noreply.github.com>
2026-07-22 18:36:27 +0100

    libretro: fix compile on android (#68)

--- io/iocore.h
+++ io/iocore.h
@@ -3,8 +3,8 @@
 
 typedef void (*FNIORESET)(const NP2CFG *pConfig);
 typedef void (*FNIOBIND)(void);
-typedef	void (IOOUTCALL *IOOUT)(UINT port, UINT val);
-typedef	UINT (IOINPCALL *IOINP)(UINT port);
+typedef void (IOOUTCALL *IOOUT)(UINT port, UINT8 val);
+typedef UINT8 (IOINPCALL *IOINP)(UINT port);
 
 #include	"lsidef.h"
 
